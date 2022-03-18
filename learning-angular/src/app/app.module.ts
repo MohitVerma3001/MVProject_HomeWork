d0febce85df9d62af0eb0a5f93aa62c9d7c29372
MohitVerma3001 <90637652+MohitVerma3001@users.noreply.github.com>
2022-03-18 03:11:11 -0400

    Interpolation

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -2,21 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
-import { TopnavComponent } from './topnav/topnav.component';
 import { DatabindComponent } from './databind/databind.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavComponent,
     DatabindComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule
