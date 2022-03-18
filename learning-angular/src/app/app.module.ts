7f38860350f6cd8e71b7bba8721c70af8d2554e4
MohitVerma3001 <90637652+MohitVerma3001@users.noreply.github.com>
2022-03-18 04:23:16 -0400

    Class and Style Binding

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DatabindComponent } from './databind/databind.component';
+import { ClassStyleComponent } from './class-style/class-style.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatabindComponent
+    DatabindComponent,
+    ClassStyleComponent
   ],
   imports: [
     BrowserModule
