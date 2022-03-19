1ab074f509065b1b233b71ca110564c0e12501d4
MohitVerma3001 <90637652+MohitVerma3001@users.noreply.github.com>
2022-03-19 05:03:28 -0400

    ngSwitch

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgswitchComponent } from './ngswitch/ngswitch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgswitchComponent,
   ],
   imports: [
     BrowserModule
