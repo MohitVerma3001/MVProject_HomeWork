8d07346d34af2e632d51a6ded5a0e44abe693e8a
MohitVerma3001 <90637652+MohitVerma3001@users.noreply.github.com>
2022-03-19 01:38:13 -0400

    Two Way Data Binding

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DatabindComponent } from './databind/databind.component';
 import { ClassStyleComponent } from './class-style/class-style.component';
 import { EventbindComponent } from './eventbind/eventbind.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatabindComponent,
     ClassStyleComponent,
-    EventbindComponent
+    EventbindComponent,
+    TwoWayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
